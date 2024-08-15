e845fe393f239eba6922267b0fda34c174d5a0b6
fahime abdollahi <famiabdollahi@gmail.com>
2024-08-15 21:41:33 +0330

    seed database for test

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    public function run()
+    {
+        Product::create([
+            'name' => 'Sample Product 1',
+            'description' => 'This is a sample product.',
+            'price' => 100,
+            'category_id' => 1, // Make sure this category exists
+            'seller_id' => 1, // Make sure this seller exists
+        ]);
+
+        Product::create([
+            'name' => 'Sample Product 2',
+            'description' => 'This is another sample product.',
+            'price' => 200,
+            'category_id' => 1,
+            'seller_id' => 1,
