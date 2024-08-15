6ada9689d84bd191a429c5442ba62ad0cc5b0048
fahime abdollahi <famiabdollahi@gmail.com>
2024-08-15 22:08:44 +0330

    previously ignored files

--- database/migrations/2024_08_15_140054_create_products_table.php
+++ database/migrations/2024_08_15_140054_create_products_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name'); // Ensure this line is present
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('seller_id')->constrained();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('products');
     }
-};
+}
